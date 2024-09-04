d9df0b5000dd317ce32388b1605f60bbf6c59376
vaibhav4399 <vaibhavprabhu19@gmail.com>
2024-09-04 12:29:59 +0530

    Initial commit with the necessary packages and the tailwind configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-extrabold text-6xl'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      screens: {
+        'xs': '475px',
+        ...defaultTheme.screens,
+      },
+      fontFamily: {
+        sans: ['Oswald', 'Roboto', 'sans-serif'], 
+      },
+      colors: {
+        bodyBackgroud: '#f8f8f8',
+        bodyBackgroundDark: '#f8f8f8'
+      },
+      width: {
+        w10: '10%',
+        w20: '20%',
+        w30: '30%',
+        w40: '40%',
+        w50: '50%',
+        w60: '60%',
+        w70: '70%',
+        w80: '80%',
+        w90: '90%',
+      },
+      height: {
+        h10: '10%',
+        h20: '20%',
+        h30: '30%',
+        h40: '40%',
+        h50: '50%',
+        h60: '60%',
+        h70: '70%',
+        h80: '80%',
+        h90: '90%',
+      }
+    },
+  },
+  plugins: [],
+}
+
